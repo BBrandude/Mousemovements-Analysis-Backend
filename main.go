96c396eafbcaa8317e9ee8b29631906a04c13ef8
BBrandude <BrandudeClutch1@gmail.com>
2022-07-19 02:42:59 -0400

    set cors

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"mouse-mousements-thesis-backend/handlers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/proccessdata", handlers.ProccessData)
 
